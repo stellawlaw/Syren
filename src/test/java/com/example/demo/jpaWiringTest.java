4db27b2c87ba7c21ac638ca860261ff205a8cfd9
Kolton Koester <koltonkoester10@gmail.com>
2020-11-23 14:40:06 -0500

    making passing api test with patients and care givers and vitals

--- src/test/java/com/example/demo/jpaWiringTest.java
+++ src/test/java/com/example/demo/jpaWiringTest.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import com.example.demo.resource.CareGiver;
 import com.example.demo.resource.Patient;
 import com.example.demo.resource.Vitals;
+import com.example.demo.storage.CareGiverRepository;
 import com.example.demo.storage.PatientRepository;
+import com.example.demo.storage.VitalRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,24 +17,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class jpaWiringTest {
     @Autowired
     private PatientRepository patientRepo;
-//    @Autowired
-//    private TestEntityManager entityManager;
-//    private void flushAndClear(){
-//        entityManager.flush();
-//        entityManager.clear();
-//    }
+    @Autowired
+    private VitalRepository vitalRepo;
+    @Autowired
+    private CareGiverRepository careGiverRepo;
+    @Autowired
+    private TestEntityManager entityManager;
+    private void flushAndClear(){
+        entityManager.flush();
+        entityManager.clear();
+    }
 
 
     @Test
     public void patientRepoShouldSaveAndRetrieveVitalObjects(){
-        Patient testPatient1 = new Patient(01234,25,"male",new Vitals(53,65,25,"1654164964","good","awesome","perfect"),"","","","","",new CareGiver(1654,65,85,35,"November 25 2020"));
+        Vitals vitals1 = new Vitals(53, 65, 25, "1654164964", "good", "awesome", "perfect");
+        vitalRepo.save(vitals1);
+        CareGiver careGiver1 = new CareGiver(1654, 65, 85, 35, "November 25 2020");
+        careGiverRepo.save(careGiver1);
+        Patient testPatient1 = new Patient(01234,25,"male", vitals1,"","","","","", careGiver1);
         patientRepo.save(testPatient1);
-//        flushAndClear();
+        flushAndClear();
         Patient retrievedPatient = patientRepo.findById(testPatient1.getId()).get();
         assertThat(retrievedPatient).isEqualTo(testPatient1);
     }
-    
 
+    @Test
+    public void patientHasOneVitalAndOneCareGiver(){
+        Vitals testVital = new Vitals(53,65,25,"1654164964","good","awesome","perfect");
+        vitalRepo.save(testVital);
+        CareGiver testcareGiver = new CareGiver(1654, 65, 85, 35, "November 25 2020");
+        careGiverRepo.save(testcareGiver);
+        Patient testPatient1 = new Patient(01234,25,"male",testVital,"","","","","", testcareGiver);
+        patientRepo.save(testPatient1);
+        flushAndClear();
+        Patient retrievedPatient1 = patientRepo.findById(testPatient1.getId()).get();
+        assertThat(retrievedPatient1.getVitals()).isEqualTo(testVital);
+        Patient retrievedpatient2 = patientRepo.findById(testPatient1.getId()).get();
+        assertThat(retrievedpatient2.getCareGiver()).isEqualTo(testcareGiver);
+    }
+    
 
 }
 
