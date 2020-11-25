609d56fbe680dedffaee936586e9c9f279ee36b6
Pete Schaaf <peteschaaf.19@gmail.com>
2020-11-25 12:22:19 -0500

    pushing to save current progress before I break something :D

--- .github/frontend/js/ems.js
+++ .github/frontend/js/ems.js
@@ -11,9 +11,3 @@ icon.addEventListener('click', () => {
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
