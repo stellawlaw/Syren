547087021aacc89b7c9d38023cf8b7d71662a4bd
Pete Schaaf <peteschaaf.19@gmail.com>
2020-12-09 15:31:05 -0500

    jUsTiFy CoNtEnT cEnTeR... Styling almost complete and buttoning up some minor details.

--- .github/frontend/js/header.js
+++ .github/frontend/js/header.js
@@ -1,12 +1,14 @@
-
-
-const createHeader = function (hospital) {
+const createHeader = function(hospital) {
     const h1 = document.createElement("h1")
-    h1.innerText = `${hospital.name} - Patient Intake List`;
-    
+    h1.classList.add("main-header")
+    h1.innerText = hospital.name;
+    const h3 = document.createElement("h3")
+    h3.innerText = "Patient Intake List";
+    h3.prepend(h1);
+
     console.log(hospital.name);
 
-    return h1;
+    return h3;
 }
 
 export {
