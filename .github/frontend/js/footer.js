c261fcb6b0a46d58a82b232500b5b2b52096e802
stella.w.law@gmail.com <stella.w.law@gmail.com>
2020-12-10 14:14:10 -0500

    styling for homepage and hospital view

--- .github/frontend/js/footer.js
+++ .github/frontend/js/footer.js
@@ -1,7 +1,7 @@
 const footer = document.createElement("footer");
 const copyright = document.createElement("p");
 copyright.classList.add("copyright");
-copyright.innerHTML = '&#169' + 'Syren'
+copyright.innerHTML = '&#169' + 'Syren 2020'
 
 const socialStack = document.createElement("div");
 socialStack.classList.add("social-stack");
@@ -21,15 +21,11 @@ githublogo.classList.add("github-logo");
 githublogo.setAttribute("src", "./imgs/github.png");
 githublogo.setAttribute("alt", "unavailable");
 
-const linkedinlogo = document.createElement("img");
-linkedinlogo.classList.add("linkedin-logo");
-linkedinlogo.setAttribute("src", "./imgs/linkedin.png");
-linkedinlogo.setAttribute("alt", "unavailable");
 
 socialStack.appendChild(fblogo);
 socialStack.appendChild(twitterlogo);
 socialStack.appendChild(githublogo);
-socialStack.appendChild(linkedinlogo);
+
 
 footer.appendChild(copyright);
 footer.appendChild(socialStack);
