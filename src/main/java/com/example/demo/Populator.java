53da8511178dec9b6abaf3d7b2a6aa62bf83b315
Kolton Koester <koltonkoester10@gmail.com>
2021-01-12 14:09:35 -0500

    added in grant hosptial lat and long

--- src/main/java/com/example/demo/Populator.java
+++ src/main/java/com/example/demo/Populator.java
@@ -26,7 +26,7 @@ public class Populator implements CommandLineRunner {
     public void run(String... args) throws Exception{
         Hospital hospital1 = new Hospital("Riverside Methodist Hospital", new Coordinates(12.1, 12.1));
         hospitalRepo.save(hospital1);
-        Hospital hospital2 = new Hospital("Grant Medical Center", new Coordinates(13.1, 13.1));
+        Hospital hospital2 = new Hospital("Grant Medical Center", new Coordinates(39.9600, 82.9911));
         hospitalRepo.save(hospital2);
         Hospital hospital3 = new Hospital("Nationwide Children's Hospital", new Coordinates(14.1, 14.1));
         hospitalRepo.save(hospital3);
